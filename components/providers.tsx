4f21090f69ccb145f240770849298c85331d07e2
PNeves10 <pedromcn2003@gmail.com>
2025-06-29 11:02:37 +0100

    Fix SessionProvider wrapper for next-auth

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,19 +1,22 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
 import { AuthProvider } from '@/contexts/auth-context';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <AuthProvider>
-        {children}
-      </AuthProvider>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
